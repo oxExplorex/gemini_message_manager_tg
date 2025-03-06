ccd6e914f33fea61f58fbff41c7685527b1b28c9
Рыжик <86830832+oxExplorex@users.noreply.github.com>
2025-03-07 00:38:42 +0700

    Pre-Alpha

--- handlers/user/business/business_handler.py
+++ handlers/user/business/business_handler.py
@@ -5,6 +5,7 @@ from aiogram.fsm.context import FSMContext
 from aiogram.types import Message
 
 from _logging import bot_logger
+from db.main import update_user, get_dump_chat_user, get_account_tg_to_user_id, create_dump_chat_user
 from loader import router, bot
 from aiogram import html
 
@@ -19,18 +20,30 @@ from aiogram import html
     ]),
     StateFilter("*"))
 async def any_message_media_handler(message: Message, state: FSMContext):
-    bot_logger.debug(message)
+    # bot_logger.debug(message)
     pass
 
 
 @router.business_message(StateFilter("*"))
 async def any_message_media_handler(message: Message, state: FSMContext):
-    _user_id = (await bot.get_business_connection(message.business_connection_id)).user.id
+    _admin_id = (await bot.get_business_connection(message.business_connection_id)).user.id
 
-    chat_id = message.chat.id
+    _chat_id = message.chat.id
     username = message.chat.username
     chat_name = message.chat.full_name or message.chat.title
 
-    bot_logger.debug(message)
+    # bot_logger.debug(message)
+
+    if not await get_dump_chat_user(_admin_id, _chat_id):
+
+        bot_logger.info(f"USER: {_admin_id} | Новый чат {_chat_id} @{username} | {chat_name}")
+        await create_dump_chat_user(_admin_id, _chat_id)
+
+        _quote = html.quote(chat_name) if chat_name else chat_name
+
+        _settings = await get_account_tg_to_user_id(_admin_id)
+        if _settings.alert_new_chat:
+            _log_chat_id = _settings.admin_id if _settings.alert_new_chat_id < 10 else _settings.alert_new_chat_id
+            await bot.send_message(_log_chat_id, f"💬 Обнаружен новый чат: {_chat_id} @{username} | {_quote}\n")
 
 
